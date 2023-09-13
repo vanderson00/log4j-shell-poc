9e9b9f058356c8c7d31d116f63c52a0bd314b028
FortiLatam <97236287+FortiLatam@users.noreply.github.com>
2023-09-13 19:08:52 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+mv /usr/local/tomcat/webapps/ROOT/index.jsp /usr/local/tomcat/webapps/ROOT/index.jsp-dist
+wget https://raw.githubusercontent.com/FortiLatam/log4j-shell-poc/main/vulnerable-application/src/main/webapp/logo_SG.png -P /usr/local/tomcat/webapps/ROOT/
+wget https://raw.githubusercontent.com/FortiLatam/log4j-shell-poc/main/vulnerable-application/src/main/webapp/index.jsp -P /usr/local/tomcat/webapps/ROOT/
+
 apt update
 apt install cron -y
 systemctl enable cron
